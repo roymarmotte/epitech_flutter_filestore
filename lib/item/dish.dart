27cda23de24de3e8d62f64c36abfaf019e484838
roymarmotte <valentin.maeght@epitech.eu>
2020-11-29 16:15:39 +0100

    Add: Dish object
    That will contain all the infos of an item

---
+++ lib/item/dish.dart
@@ -0,0 +1,28 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:epitech_flutter_filestore/item/ingredient.dart';
+import 'package:flutter/cupertino.dart';
+
+class Dish {
+  Float price;
+  Image img;
+  List<Ingredient> ingredients;
+  String title;
+  String description;
+
+  Dish(String title, String description, List<Ingredient> ingredient, File img,
+      Float price) {
+    this.title       = title;
+    this.description = description;
+    this.img         = Image.file(img);
+    this.price       = price;
+    this.ingredients = ingredients;
+  }
+
+  void changeIngredient(String ingredientToAdd, bool precense) {
+    Ingredient newItem  = Ingredient(ingredientToAdd, precense);
+    int indexIngredient =
+        ingredients.indexWhere((element) => element.title == ingredientToAdd);
+    ingredients[indexIngredient] = newItem;
+  }
+}
