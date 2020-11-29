dae7f5166c0a48a0d14eac1b2821b09672f6c077
roymarmotte <valentin.maeght@epitech.eu>
2020-11-29 19:33:02 +0100

    Format: Dish, Ingredient and DishComponent

--- lib/items/dish.dart
+++ lib/items/dish.dart
@@ -7,17 +7,17 @@ class Dish {
   String title;
   String description;
 
-  Dish(String title, String description, List<Ingredient> ingredient, String img,
-      double price) {
-    this.title       = title;
+  Dish(String title, String description, List<Ingredient> ingredient,
+      String img, double price) {
+    this.title = title;
     this.description = description;
-    this.img         = img;
-    this.price       = price;
+    this.img = img;
+    this.price = price;
     this.ingredients = ingredients;
   }
 
   void changeIngredient(String ingredientToAdd, bool precense) {
-    Ingredient newItem  = Ingredient(ingredientToAdd, precense);
+    Ingredient newItem = Ingredient(ingredientToAdd, precense);
     int indexIngredient =
         ingredients.indexWhere((element) => element.title == ingredientToAdd);
     ingredients[indexIngredient] = newItem;
