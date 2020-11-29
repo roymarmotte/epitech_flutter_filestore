2d849546d89920af7efad84770384d909abd51c0
Halters <38920575+Halters@users.noreply.github.com>
2020-11-29 21:41:08 +0100

    Add home page (#3)
    * [ADD] HomePage Structure
    
    * [MOD] Change Icon on AnimatedBottomNavigatedBar
    
    * Item dish (#2)
    
    * Add: ingredient class
    
    This will allow the user to customise his dish
    
    * Add: presence in Ingredient
    
    and delete price, that is a useless variable
    
    * Del: Useless import
    
    * Add: Dish object
    
    That will contain all the infos of an item
    
    * Change: folder item => items
    
    * Add: dishComponent
    
    Widget that will be a ListTile for the ListItem in the homepage
    
    * Change: image in dish
    
    * Change: Image of DishComponent less big
    
    * Add: CI for flutter
    
    * Create flutter.yml
    
    * Update dart.yml
    
    * Add: Flutter CI
    
    * Update: flutter.yml
    
    * Update: Use Prettier in CI
    
    * Revert "Update: Use Prettier in CI"
    
    This reverts commit 87d4472980a9dd870e45daa10fa700beec5307f4.
    
    * Del: CI, flutter format flag
    
    * Del: useless import in Dish
    
    * Format: Dish, Ingredient and DishComponent
    
    * Delete: useless on pull request
    
    * [ADD] HomePage Structure
    
    * [MOD] Change Icon on AnimatedBottomNavigatedBar
    
    * Add: ListTile on home-page
    
    * Del: Old unit test not useful anymore
    
    Co-authored-by: Valentin Maeght <36154789+roymarmotte@users.noreply.github.com>
    Co-authored-by: roymarmotte <valentin.maeght@epitech.eu>

---
+++ lib/page/home-page.dart
@@ -0,0 +1,141 @@
+import 'package:epitech_flutter_filestore/components/dishComponent.dart';
+import 'package:epitech_flutter_filestore/items/dish.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _bottomNavIndex = 0;
+  final iconList = <IconData>[
+    Icons.home,
+    Icons.account_circle_outlined,
+    Icons.shopping_basket_outlined,
+  ];
+
+  void shoppingBasketRoad() {
+    print("Shopping basket icon clicked !");
+  }
+
+  void searchButtonRoad() {}
+
+  @override
+  Widget build(BuildContext context) {
+    List<Dish> initialDish = [
+      Dish(
+          "Omelette au fromage",
+          "It's very good !",
+          List(),
+          "https://www.canalvie.com/polopoly_fs/1.8675792!/image/Omelette%20fromage.jpg_gen/derivatives/cvlandscape_499_281/Omelette%20fromage.jpg",
+          8.00),
+      Dish(
+          "Welsh",
+          "It's very very good !",
+          List(),
+          "https://img.cuisineaz.com/660x660/2019-08-08/i149772-welsh-au-thermomix.jpeg",
+          9.20),
+    ];
+    List<Widget> widgetDish = initialDish.map((e) => DishComponent(e)).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: new Center(
+          child: Text(
+            "Flutter File Store",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width * 0.80,
+                height: MediaQuery.of(context).size.height *
+                    0.10, //MediaQuery.of(context).size.height % 10,
+                child: TextField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                      hintText: "Search a dish or an entrée...",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90))),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 50.0),
+                child: Text(
+                  'Choose a Categorie :',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.80,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Wrap(
+                      spacing: 10,
+                      children: <Widget>[
+                        Chip(label: const Text("Entrée")),
+                        Chip(label: const Text("Plat Chauds")),
+                        Chip(label: const Text("Plats Froids")),
+                        Chip(label: const Text("Desserts"))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          ...widgetDish,
+        ],
+      ),
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        backgroundColor: HexColor('#4285F4'),
+        gapLocation: GapLocation.none,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 0,
+        rightCornerRadius: 0,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
+      ),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
