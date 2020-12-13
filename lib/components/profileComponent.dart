11fe63d3f3b7e12f60ad579557ec6ba367640d85
H.Tnacheri <hicham.tnacheri@epitech.eu>
2020-12-13 22:53:36 +0100

    [mod] Remove unsued import

--- lib/components/profileComponent.dart
+++ lib/components/profileComponent.dart
@@ -1,4 +1,3 @@
-import 'package:epitech_flutter_filestore/items/dish.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
