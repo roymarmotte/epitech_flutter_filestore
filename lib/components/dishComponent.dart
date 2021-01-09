ec967c2d1c80f545db2919ff516ecd7771044929
Louis <louis.lefebvre@epitech.eu>
2021-01-09 18:10:47 +0100

    feat: update dish component design

--- lib/components/dishComponent.dart
+++ lib/components/dishComponent.dart
@@ -10,23 +10,65 @@ class DishComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: ListTile(
-            leading: Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: NetworkImage(this.dish.img), fit: BoxFit.fill),
-              ),
-            ),
-            title: Text(this.dish.title),
-            subtitle: Text(this.dish.description),
-            isThreeLine: true,
-            trailing: Icon(Icons.arrow_forward_ios),
+        elevation: 0,
+        child: InkWell(
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => DishesPage(dish)));
-            }));
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: NetworkImage(dish.img),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  height: 180.0, // Card Image Container size
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 4.5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        flex: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(dish.title,
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                )),
+                            Text(
+                              dish.description,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          dish.price.toString() + " €",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 }
